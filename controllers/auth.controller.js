5f724c9faa6f6ba5348d482e65ebe2510be6e289
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-19 15:48:28 +0300

    added sign up and sign in apis

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,25 +1,33 @@
-// const connection = require("../configs/db.connection");
+const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const login = (req, res) => {
+
+const login = async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) res.status(401).send({ message: "Email and Password are required" })
-  connection.query("SELECT * FROM USERS WHERE email = ?", [email], async (err, result) => {
-    if (err) return res.status(500).send({ message: "Something wrong happened" })
-    if (result.length !== 1) return res.status(401).send({ message: " Incorrect Email/Password" })
-    const user = result[0];
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) return res.status(401).send({ message: "Email and Password are required" })
+  if (!email || !password) return res.status(401).send({ message: "Email and Password are required" })
 
-    const { password: hashedPassword, ...userInfo } = user
+  try {
+  const user = await User.findOne({email}).lean()
+  
+  if (!user){
+    return res.status(401).send({ message: "Email not found" })
+  }
 
-    const token = jwt.sign(userInfo, process.env.SECRET_KEY)
+ const isValid = await bcrypt.compare(password, user.password);
+ if (!isValid) return res.status(401).send({ message: "Email and Password are required" })
+ const { password: hashedPassword, ...userInfo } = user
+
+const token =  jwt.sign(userInfo, process.env.SECRET_KEY)
     return res.send({
       token,
       user: userInfo
-    })
-  })
+    }) 
+  
+}catch(err){
+  console.err
+}
+
 }
 
 module.exports = { login }
